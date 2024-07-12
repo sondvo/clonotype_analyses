a885f58ad6e65a061d8290ff5237acd4c343d085
Son Vo <duyson1999@gmail.com>
2024-07-13 01:05:06 +0700

    finish

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # clonotype_analyses
-Processing, analyzing, and visualizing TCR/BCR 
+Processing, analyzing, and visualizing TCR/BCR
 
 ## 1. GSE185381
 - Link: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE185381
-- Used datasets: ***_vdj_t_**filtered**_contig_annotations.csv.gz
+- Used datasets: xxx_vdj_t_**filtered**_contig_annotations.csv.gz
+- Finish plotly and dash
+
+![alt text](plotly_html.gif)
--- html_plot.py
+++ html_plot.py
@@ -2,8 +2,8 @@ import plotly.express as px
 from dash import html
 from dash import dcc
 import dash
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from dash.dependencies import Input, Output, State
 import datetime as dt
 
@@ -83,7 +83,7 @@ reformated_cdr3_beta_length_df = pd.DataFrame(reformated_cdr3_beta_length_dct)
 # UMAP_scatter_pie = pd.read_csv(DATA_DIR + '/UMAP_porportion.tsv', sep='\t')
 
 
-app = dash.Dash(__name__) 
+app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 
 fig1 = px.scatter(
@@ -91,9 +91,9 @@ fig1 = px.scatter(
     x='X_UMAP',
     y='Y_UMAP',
     title='UMAP 1',
-    color="Author's cell type", 
+    color="Author's cell type",
     labels={'color': "Author's cell type"},
-    height=500, 
+    height=500,
     width=900,
 )
 fig1.update_traces(marker={'size': 5})
@@ -104,9 +104,9 @@ fig11 = px.scatter(
     x='X_UMAP',
     y='Y_UMAP',
     title='UMAP 2',
-    color="simplified_celltype", 
+    color="simplified_celltype",
     labels={'color': "simplified_celltype"},
-    height=500, 
+    height=500,
     width=900,
 )
 fig11.update_traces(marker={'size': 5})
@@ -117,9 +117,9 @@ fig12 = px.scatter(
     x='X_UMAP',
     y='Y_UMAP',
     title='UMAP 3',
-    color="Condition", 
+    color="Condition",
     labels={'color': "Condition"},
-    height=500, 
+    height=500,
     width=900,
 )
 fig12.update_traces(marker={'size': 5})
@@ -130,9 +130,9 @@ fig13 = px.scatter(
     x='X_UMAP',
     y='Y_UMAP',
     title='UMAP 4',
-    color="Subject ID", 
+    color="Subject ID",
     labels={'color': "Subject ID"},
-    height=500, 
+    height=500,
     width=900,
     category_orders={"Subject ID": sorted(clinical_meta['Subject ID'].unique())[::-1]}
 )
@@ -141,17 +141,17 @@ fig13.update_layout(margin_t=50, margin_r=300)
 
 
 fig2 = px.bar(
-    reformated_single_ambiguos_clonotypes_df, 
+    reformated_single_ambiguos_clonotypes_df,
     x='subject id',
-    y="ratio", 
+    y="ratio",
     title='''Ratio of Single / Ambiguous Clonotypes<br><br>
     <sup>A proper clonotype is defined as having only 1 TRA and 1 TRB. However, other types of ambiguous clonotypes might also show some clinical significance.<br>
     From the picture, the number of T cells with only a single chain TRB is abundant across patients.
     </sup>
     ''',
-    color="clonotypes types", 
+    color="clonotypes types",
     barmode='stack',
-    height=700, 
+    height=700,
     width=1000,
 )
 fig2.update_layout(margin_t=150)
@@ -172,27 +172,27 @@ fig3 = px.box(
 fig3.update_layout(margin_t=150)
 
 fig4 = px.bar(
-    reformated_clonotypes_enrichment_df, 
-    x="subject id", 
-    y="ratio", 
+    reformated_clonotypes_enrichment_df,
+    x="subject id",
+    y="ratio",
     title='''Clononal Expansion<br><br>
     <sup> TCR clonal expansion across normal and Acute Myeloid Leukocytes patients.<br>
     Although most cells contains unique TCR, AML patients show higher degree of expanded clonotype - clonal expansion (n>5)
     </sup>
     ''',
-    color="Clonotypes size", 
+    color="Clonotypes size",
     barmode = 'stack',
-    height=700, 
+    height=700,
     width=1000,
 )
 fig4.update_layout(margin_t=150)
 
 fig5 = px.line(
-    reformated_cdr3_alpha_length_df, 
-    x="CDR3 length", 
-    y="Ratio", 
-    color="Condition", 
-    height=500, 
+    reformated_cdr3_alpha_length_df,
+    x="CDR3 length",
+    y="Ratio",
+    color="Condition",
+    height=500,
     width=800,
     title='CDR3 - Alpha Length across Conditions',
     markers=True
@@ -201,11 +201,11 @@ fig5.update_layout(yaxis_range=[0, 0.35])
 fig5.update_layout(margin_t=50)
 
 fig6 = px.line(
-    reformated_cdr3_beta_length_df, 
-    x="CDR3 length", 
-    y="Ratio", 
-    color="Condition", 
-    height=500, 
+    reformated_cdr3_beta_length_df,
+    x="CDR3 length",
+    y="Ratio",
+    color="Condition",
+    height=500,
     width=800,
     title='CDR3 - Beta Length across Conditions',
     markers=True
@@ -216,7 +216,7 @@ fig6.update_layout(margin_t=50)
 app.layout = html.Div(
     children=[
         html.H1(
-            'GSE185381 - TCR ANALYSIS', 
+            'GSE185381 - TCR ANALYSIS',
             style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}
         ),
         html.Div([
