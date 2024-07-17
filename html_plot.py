9eb8112e67b553ddafbd87da947cf0c877e0cdb9
Son Vo <duyson1999@gmail.com>
2024-07-17 12:12:03 +0700

    plotly_html

--- html_plot.py
+++ html_plot.py
@@ -9,75 +9,23 @@ import datetime as dt
 
 
 DATA_DIR = 'GSE185381_TCR/clonotypes'
+PLOTLY_DATA_DIR = DATA_DIR + '/plotly_data'
 
 # fig1: umap, Author's cell types
-clinical_meta = pd.read_csv('GSE185381_TCR/clinical_metadata.tsv', sep='\t', index_col=0)
+clinical_meta = pd.read_csv('GSE185381_TCR/clinical_metadata.tsv', sep='\t')
 
 # fig2: single, ambigous clonotypes proportion
-single_ambiguos_clonotypes = pd.read_csv(DATA_DIR + '/fraction_clo_df.tsv', sep='\t', index_col=0)
-reformated_single_ambiguos_clonotypes_dct = {
-    'subject id': [],
-    'clonotypes types': [],
-    'ratio': []
-}
-for i in single_ambiguos_clonotypes.index:
-    for j in single_ambiguos_clonotypes.columns:
-        reformated_single_ambiguos_clonotypes_dct['subject id'].append(i)
-        reformated_single_ambiguos_clonotypes_dct['clonotypes types'].append(j)
-        reformated_single_ambiguos_clonotypes_dct['ratio'].append(
-            single_ambiguos_clonotypes.loc[i, j]
-        )
-reformated_single_ambiguos_clonotypes_df = pd.DataFrame(reformated_single_ambiguos_clonotypes_dct)
+reformated_single_ambiguos_clonotypes_df = pd.read_csv(PLOTLY_DATA_DIR + '/fraction_clo_df.tsv', sep='\t')
 
 # fig3: clonotypes diversity estimation: Shannon entropy
-shannon_df = pd.read_csv(DATA_DIR + '/shannon_df.tsv', sep='\t', index_col=0)
+shannon_df = pd.read_csv(PLOTLY_DATA_DIR + '/shannon_df.tsv', sep='\t')
 
 # fig4: clonotypes expansion:
-clonotypes_expansion = pd.read_csv(DATA_DIR + '/clono_expansion_df.tsv.tsv', sep='\t', index_col=0)
-reformated_clonotypes_expansion_dct = {
-    'subject id': [],
-    'Clonotypes size': [],
-    'ratio': []
-}
-for i in clonotypes_expansion.index:
-    for j in clonotypes_expansion.columns:
-        reformated_clonotypes_expansion_dct['subject id'].append(i)
-        reformated_clonotypes_expansion_dct['Clonotypes size'].append(j)
-        reformated_clonotypes_expansion_dct['ratio'].append(
-            clonotypes_expansion.loc[i, j]
-        )
-reformated_clonotypes_expansion_df = pd.DataFrame(reformated_clonotypes_expansion_dct)
+reformated_clonotypes_expansion_df = pd.read_csv(PLOTLY_DATA_DIR + '/clono_expansion_df.tsv', sep='\t')
 
 # fig5: cdr3 alpha, beta length:
-cdr3_alpha_length = pd.read_csv(DATA_DIR + '/cdr3alpha_length_df.tsv', sep='\t', index_col=0)
-reformated_cdr3_alpha_length_dct = {
-    'Condition': [],
-    'CDR3 length': [],
-    'Ratio': []
-}
-for i in cdr3_alpha_length.index:
-    for j in cdr3_alpha_length.columns:
-        reformated_cdr3_alpha_length_dct['Condition'].append(i)
-        reformated_cdr3_alpha_length_dct['CDR3 length'].append(j)
-        reformated_cdr3_alpha_length_dct['Ratio'].append(
-            cdr3_alpha_length.loc[i, j]
-        )
-reformated_cdr3_alpha_length_df = pd.DataFrame(reformated_cdr3_alpha_length_dct)
-
-cdr3_beta_length = pd.read_csv(DATA_DIR + '/cdr3beta_length_df.tsv', sep='\t', index_col=0)
-reformated_cdr3_beta_length_dct = {
-    'Condition': [],
-    'CDR3 length': [],
-    'Ratio': []
-}
-for i in cdr3_beta_length.index:
-    for j in cdr3_beta_length.columns:
-        reformated_cdr3_beta_length_dct['Condition'].append(i)
-        reformated_cdr3_beta_length_dct['CDR3 length'].append(j)
-        reformated_cdr3_beta_length_dct['Ratio'].append(
-            cdr3_beta_length.loc[i, j]
-        )
-reformated_cdr3_beta_length_df = pd.DataFrame(reformated_cdr3_beta_length_dct)
+reformated_cdr3_alpha_length_df = pd.read_csv(PLOTLY_DATA_DIR + '/cdr3alpha_length_df.tsv', sep='\t')
+reformated_cdr3_beta_length_df = pd.read_csv(PLOTLY_DATA_DIR + '/cdr3beta_length_df.tsv', sep='\t')
 
 # # fig6: UMAP simplified cell type + UMAP condition proportions
 # UMAP_scatter_pie = pd.read_csv(DATA_DIR + '/UMAP_porportion.tsv', sep='\t')
@@ -142,14 +90,14 @@ fig13.update_layout(margin_t=50, margin_r=300)
 
 fig2 = px.bar(
     reformated_single_ambiguos_clonotypes_df,
-    x='subject id',
-    y="ratio",
+    x='Subjects',
+    y="Ratio",
     title='''Ratio of Single / Ambiguous Clonotypes<br><br>
     <sup>A proper clonotype is defined as having only 1 TRA and 1 TRB. However, other types of ambiguous clonotypes might also show some clinical significance.<br>
     From the picture, the number of T cells with only a single chain TRB is abundant across patients.
     </sup>
     ''',
-    color="clonotypes types",
+    color="Clonotypes types",
     barmode='stack',
     height=700,
     width=1000,
@@ -159,7 +107,7 @@ fig2.update_layout(margin_t=150)
 fig3 = px.box(
     shannon_df,
     x='Condition',
-    y='entropy per patient',
+    y='Entropy per Subject ID',
     title='''Clonotypes Diversity Estimation<br><br>
     <sup>A higher Shannon entropy value indicates a more diverse population. <br>
     A lower Shannon entropy value in AML disease suggests that the body favors specific types of clonotypes to combat the disease.
@@ -176,8 +124,8 @@ fig3.update_traces(
 
 fig4 = px.bar(
     reformated_clonotypes_expansion_df,
-    x="subject id",
-    y="ratio",
+    x="Groups",
+    y="Ratio",
     title='''Clononal Expansion<br><br>
     <sup> TCR clonal expansion across normal and Acute Myeloid Leukocytes patients.<br>
     Although most cells contains unique TCR, AML patients show higher degree of expanded clonotype - clonal expansion (n>5)
@@ -190,6 +138,10 @@ fig4 = px.bar(
 )
 fig4.update_layout(margin_t=150)
 
+max_cdr3_ratio = max(
+    max(reformated_cdr3_alpha_length_df['Ratio'].values),
+    max(reformated_cdr3_beta_length_df['Ratio'].values),
+)
 fig5 = px.line(
     reformated_cdr3_alpha_length_df,
     x="CDR3 length",
@@ -200,7 +152,7 @@ fig5 = px.line(
     title='CDR3 - Alpha Length across Conditions',
     markers=True
 )
-fig5.update_layout(yaxis_range=[0, 0.35])
+fig5.update_layout(yaxis_range=[0, max_cdr3_ratio + 0.05])
 fig5.update_layout(margin_t=50)
 
 fig6 = px.line(
@@ -213,7 +165,7 @@ fig6 = px.line(
     title='CDR3 - Beta Length across Conditions',
     markers=True
 )
-fig6.update_layout(yaxis_range=[0, 0.35])
+fig6.update_layout(yaxis_range=[0, max_cdr3_ratio + 0.05])
 fig6.update_layout(margin_t=50)
 
 app.layout = html.Div(
