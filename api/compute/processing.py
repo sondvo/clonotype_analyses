0204eb3ba2aab8b159fe7af4a35f834ce7e5018f
Son Vo <duyson1999@gmail.com>
2024-07-31 14:31:02 +0700

    structure

---
+++ api/compute/processing.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from .. import common
+from constants import VDJ_10X_CHOSEN_COLUMNS
+
+
+def _preprocessing_clonotypes(df):
+	df['raw_clonotype_id'] = df['raw_clonotype_id'].fillna('None')
+	df = df.iloc[
+		df['raw_clonotype_id'].values != 'None',
+		:
+	]
+	return df
+
+
+def reformat_clonotypes(vdj_path, preprocessing=True):
+	chosen_columns = [i.value for i in VDJ_10X_CHOSEN_COLUMNS]
+
+	vdj_df = common.read_csv(vdj_path)
+	vdj_df.columns = [i.lower() for i in vdj_df.columns]
+
+	try:
+		vdj_df = vdj_df[chosen_columns]
+	except Exception as e:
+		print (e)
+
+	if preprocessing:
+		return _preprocessing_clonotypes(vdj_df)
+
+	return vdj_df
+
+
+def merge_with_clinical_meta(vdj_df, meta_df):
+	print ('WARNING: only keep intersect barcodes')
+	vdj_original_bc = vdj_df[VDJ_10X_CHOSEN_COLUMNS.BARCODES.value].values
+	bc_idx = common.matching_barcodes_idx(
+		vdj_original_bc,
+		meta_df.index.values
+	)
+	matched_bc = np.unique(vdj_original_bc[bc_idx])
+	vdj_df = vdj_df.iloc[
+		bc_idx,
+		:
+	]
+	meta_df = meta_df.loc[
+		matched_bc,
+		:
+	]
+	return vdj_df, meta_df
+
+
+def write_hdf5_vdj(vdj_df, output_h5_path):
+	with common.H5AtomicWriter(output_h5_path) as f:
+		for i in vdj_df.columns:
+			try:
+				f.create_dataset(i, data=vdj_df[i].values)
+			except:
+				f.create_dataset(i, data=vdj_df[i].values.astype('S'))
+	return output_h5_path
---
+++ api/constants.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class VDJ_10X_CHOSEN_COLUMNS(Enum):
+	BARCODES='barcodes'
+	IS_CELL='is_cell'
+	HIGH_CONFIDENCE='high_confidence'
+	LENGTH='length'
+	CHAIN='chain'
+	V_GENE='v_gene'
+	D_GENE='d_gene'
+	J_GENE='j_gene'
+	C_GENE='c_gene'
+	FULL_LENGTH='full_length'
+	PRODUCTIVE='productive'
+	CDR3='cdr3'
+	UMIS='umis'
+	RAW_CLONOTYPE_ID='raw_clonotype_id'
+	RAW_CONSENSUS_ID='raw_consensus_id'
+	EXACT_SUBCLONOTYPE_ID='exact_subclonotype_id'
+
