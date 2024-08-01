c9f6a8eab22cae11298b3ccf1d5abdf081e038ab
Son Vo <duyson1999@gmail.com>
2024-08-01 16:32:03 +0700

    save both vdj and clinical meta as h5 files

--- api/analyses.py
+++ api/analyses.py
@@ -9,6 +9,7 @@ from .compute import clonotypes_QC_fraction
 from .compute import clonotypes_diversity_rate
 
 from . import common
+from .common.constants import SAMPLE_NAME
 from .common.constants import VDJ_10X_COLUMNS
 
 
@@ -16,7 +17,8 @@ class ClonotypePreprocessing(object):
 	def __init__(
 		self,
 		batch_info: List[Dict],
-		ouput_h5_path: str,
+		ouput_vdj_h5_path: str,
+		ouput_clinical_h5_path: str,
 		preprocessing: bool = True,
 	):
 		"""
@@ -26,7 +28,7 @@ class ClonotypePreprocessing(object):
 		----------
 		batch_info : List[dict]
 			Allow merging multiple VDJ samples\n
-			For each sample, barcodes of vdj_df and clinical_meta_df MUST have similar items \n
+			For each sample: barcodes of vdj_df and clinical_meta_df MUST have similar items \n
 			Example:
 				[{
 					'sample_name': 'sample_1',\n
@@ -34,15 +36,29 @@ class ClonotypePreprocessing(object):
 					'clinical_meta_path': 'root/fol/GSM123_clinical_meta.tsv',
 				}, {...}]
 		"""
+		print ('NOTE: barcodes (first column) of vdj and clinical_meta MUST have similar items')
 		self.__batch_info = batch_info
-		self.__ouput_h5_path = ouput_h5_path
+		self.__ouput_vdj_h5_path = ouput_vdj_h5_path
+		self.__ouput_clinical_h5_path = ouput_clinical_h5_path
 		self.__preprocessing = preprocessing
+		self.__sample_specific_columns = [
+			VDJ_10X_COLUMNS.BARCODES.value,
+			VDJ_10X_COLUMNS.RAW_CLONOTYPE_ID.value,
+			VDJ_10X_COLUMNS.RAW_CONSENSUS_ID.value
+		]
 
 
 	def processing_vdj(self):
 		"""
 		Only keep necessary columns in VDJ file\n
-		Match barcodes between VDJ and clinical metadata
+		Optional: Only keep shared barcodes between VDJ and clinical metadata
+
+		Returns
+		----------
+		vdj_df : pandas.dataframe
+			No index column
+		clinical_meta_df : pandas dataframe
+			Barcodes as index column
 		"""
 		final_vdj_df = None
 		final_clinical_df = None
@@ -55,36 +71,50 @@ class ClonotypePreprocessing(object):
 			clinical_df = common.read_csv(clinical_path, index_col=0)
 
 			if len(prefix):
-				vdj_df[
-					VDJ_10X_COLUMNS.BARCODES.value
-				] = prefix + '_' + vdj_df[VDJ_10X_COLUMNS.BARCODES.value].values
-				vdj_df['sample_name'] = prefix
+				for col in self.__sample_specific_columns:
+					vdj_df[col] = prefix + '_' + vdj_df[col].values
+				vdj_df[SAMPLE_NAME] = prefix
 
 				clinical_df.index = prefix + '_' + clinical_df.index.values
-				clinical_df['sample_name'] = prefix
+				clinical_df[SAMPLE_NAME] = prefix
 
 			final_vdj_df = common.concat_df([final_vdj_df, vdj_df], axis=0)
 			final_clinical_df = common.concat_df([final_clinical_df, clinical_df], axis=0)
 		return final_vdj_df, final_clinical_df
 
 
+	## For clonotypes, each function require some spefic columns, not all of them. Eg: shannon entropy: [barcodes, raw_clonotype_id]
+	## -> save each column as key in hdf5 -> optimize data on memory
+	## Why not setting barcodes as the outer-most layer? Eg: f['ATGCAT...'].keys() = ['TRA', 'TRB', 'cdr3', ...] ?
+	## -> very few functions require picking some specific barcodes -> not optimize for analyses.
 	def ingest_data(
 			self,
-			output_clincal_path: str = None,
+			intersect_barcodes: bool = False,
 		):
 		"""
 		Save VDJ as h5 format\n
 		Keep clinical metadata as pandas dataframe
+
+		Parameters
+		----------
+		# TODO
+
+		Returns
+		----------
+		# TODO
 		"""
 		vdj_df, clinical_df = self.processing_vdj()
-		vdj_df, clinical_df = processing.matching_barcodes(
-			vdj_df, clinical_df
-		)
-		processing.write_hdf5_vdj(vdj_df, self.__ouput_h5_path)
-		common.write_csv(clinical_df, output_clincal_path)
+
+		if intersect_barcodes:
+			vdj_df, clinical_df = processing.matching_barcodes(
+				vdj_df, clinical_df
+			)
+
+		processing.write_hdf5_vdj(vdj_df, self.__ouput_vdj_h5_path)
+		processing.write_hdf5_vdj(clinical_df, self.__ouput_clinical_h5_path)
 		return {
-			'10X_VDJ': self.__ouput_h5_path,
-			'clinical_meta': output_clincal_path
+			'10X_VDJ': self.__ouput_vdj_h5_path,
+			'clinical_meta': self.__ouput_clinical_h5_path
 		}
 
 
@@ -92,19 +122,18 @@ class ClonotypeToolkits(object):
 	def __init__(
 		self,
 		vdj_h5_path: str,
-		clinical_meta_path: str,
+		clinical_h5_path: str,
 	):
 		"""
+		Load necessary files for analyses\n
+		NOTE: vdj_h5_path, clinical_h5_path are the outputs from ClonotypePreprocessing.ingest_data()
+
 		Parameters
 		----------
-		vdj_path : List[str]
+		vdj_h5_path : str
 			The path to ORIGINAL 10X VDJ files (could be multiple files for multiple samples)
-		clinical_meta_path : str
+		clinical_h5_path : str
 			The path to associate metadata file (eg. include 'condition', 'cell types', ...)\n
-			First column must be barcodes\n
-			First row must be field names
 		"""
 		self.__vdj_h5_path = vdj_h5_path
-		self.__clinical_meta = None
-		if clinical_meta_path is not None:
-			self.__clinical_meta = common.read_csv(clinical_meta_path, index_col=0)
+		self.__clinical_h5_path = clinical_h5_path
