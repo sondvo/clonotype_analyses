0d3f102f3393d5633e23e8df0bf2fb059fe07df4
Son Vo <duyson1999@gmail.com>
2024-08-02 11:16:21 +0700

    clonotype qc fraction api

--- api/analyses.py
+++ api/analyses.py
@@ -35,6 +35,9 @@ class ClonotypePreprocessing(object):
 					'vdj_path': 'root/fol/GSM123_vdj_t_filtered_contig_annotations.csv',\n
 					'clinical_meta_path': 'root/fol/GSM123_clinical_meta.tsv',
 				}, {...}]
+		ouput_vdj_h5_path : str
+		ouput_clinical_h5_path : str
+		preprocessing : boolean
 		"""
 		print ('NOTE: barcodes (first column) of vdj and clinical_meta MUST have similar items')
 		self.__batch_info = batch_info
@@ -42,7 +45,6 @@ class ClonotypePreprocessing(object):
 		self.__ouput_clinical_h5_path = ouput_clinical_h5_path
 		self.__preprocessing = preprocessing
 		self.__sample_specific_columns = [
-			VDJ_10X_COLUMNS.BARCODES.value,
 			VDJ_10X_COLUMNS.RAW_CLONOTYPE_ID.value,
 			VDJ_10X_COLUMNS.RAW_CONSENSUS_ID.value
 		]
@@ -73,6 +75,7 @@ class ClonotypePreprocessing(object):
 			if len(prefix):
 				for col in self.__sample_specific_columns:
 					vdj_df[col] = prefix + '_' + vdj_df[col].values
+				vdj_df.index = prefix + '_' + vdj_df.index.values
 				vdj_df[SAMPLE_NAME] = prefix
 
 				clinical_df.index = prefix + '_' + clinical_df.index.values
@@ -137,3 +140,56 @@ class ClonotypeToolkits(object):
 		"""
 		self.__vdj_h5_path = vdj_h5_path
 		self.__clinical_h5_path = clinical_h5_path
+
+
+	def _prepare_clonotypes_QC_fraction(self, meta_keys):
+		if not len(meta_keys):
+			raise Exception('Need at least one meta column to compare')
+
+		vdj_df = processing.h5_to_pandas(self.__vdj_h5_path, [VDJ_10X_COLUMNS.CHAIN.value])
+		clinical_df = processing.h5_to_pandas(self.__clinical_h5_path, meta_keys)
+		vdj_df, clinical_df = processing.matching_barcodes(vdj_df, clinical_df)
+		merged_df = processing.merge_vdj_and_clinical_meta(vdj_df, clinical_df)
+		fraction_clo_df, merged_meta_key = clonotypes_QC_fraction.create_clonotype_fraction_df(merged_df, meta_keys)
+		return fraction_clo_df, merged_meta_key
+
+
+	def matplotlib_clonotypes_QC_fraction(self, meta_keys=[]):
+		"""
+		Visualize fraction of paired / ambiguous clonotypes separated by metadata fields\n
+
+		Parameters
+		----------
+		meta_keys : List[str]
+			List of metadata keys for grouping \n
+			If more than one keys provided, the values will be combined together \n
+			Eg: ['condition', 'patient']
+
+		Returns
+		----------
+		Matplotlib image
+		"""
+		fraction_clo_df, merged_meta_key = self._prepare_clonotypes_QC_fraction(meta_keys)
+		return clonotypes_QC_fraction.visualize_ratio_clonotype_types(fraction_clo_df, merged_meta_key)
+
+
+	def plotly_clonotypes_QC_fraction(self, meta_keys=[]):
+		"""
+		Produce dataframe for plotly: fraction of paired / ambiguous clonotypes separated by metadata fields\n
+
+		Parameters
+		----------
+		meta_keys : List[str]
+			List of metadata keys for grouping \n
+			If more than one keys provided, the values will be combined together \n
+			Eg: ['condition', 'patient']
+
+		Returns
+		----------
+		Pandas dataframe using for plotly
+		"""
+		fraction_clo_df, merged_meta_key = self._prepare_clonotypes_QC_fraction(meta_keys)
+		return clonotypes_QC_fraction.plotly_ratio_clonotype_types(fraction_clo_df, merged_meta_key)
+
+
+
